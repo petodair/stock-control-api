b1641efbcefb2f301b479763cf2e6dfc69bb0ef9
Peterson Odair <petersomodair@gmail.com>
2026-02-02 12:51:22 -0300

    commit inicial

---
+++ src/main/java/br/com/stock_control_api/StockControlApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.stock_control_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/stock_control_api/StockControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stock_control_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
