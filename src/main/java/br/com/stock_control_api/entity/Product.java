cd94340ba62a67ac15878abb6516106d851cc537
Peterson Odair <petersomodair@gmail.com>
2026-02-02 13:08:15 -0300

    feat: criacao de um POJO da entidade Product e do enum ProductType

---
+++ src/main/java/br/com/stock_control_api/entity/Product.java
@@ -0,0 +1,20 @@
+package br.com.stock_control_api.entity;
+
+import br.com.stock_control_api.enums.ProductType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private String code;
+    private BigDecimal price;
+    private ProductType type;
+}
---
+++ src/main/java/br/com/stock_control_api/enums/ProductType.java
@@ -0,0 +1,9 @@
+package br.com.stock_control_api.enums;
+
+public enum ProductType {
+    CHICKEN,
+    BOVINE,
+    PIG,
+    VACUUM,
+    FROZEN
+}
