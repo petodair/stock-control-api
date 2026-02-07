fa5fe89aea74308e8f66213041e1872ab34eb637
Peterson Odair <petersomodair@gmail.com>
2026-02-07 10:28:35 -0300

    feat: mapeamento da entidade produto

--- src/main/java/br/com/stock_control_api/entity/Product.java
+++ src/main/java/br/com/stock_control_api/entity/Product.java
@@ -1,6 +1,7 @@
 package br.com.stock_control_api.entity;
 
 import br.com.stock_control_api.enums.ProductType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,10 +12,23 @@ import java.math.BigDecimal;
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String code;
+
+    @Column
     private BigDecimal price;
+
+    @Column(nullable = false)
     private ProductType type;
 }
