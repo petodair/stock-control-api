c8497844284ea424fe396285be3d2f22f33017aa
Peterson Odair <petersomodair@gmail.com>
2026-03-09 15:40:56 -0300

    feat: adicionando host nas configuracoes do cors

--- src/main/java/br/com/stock_control_api/configuration/CorsConfig.java
+++ src/main/java/br/com/stock_control_api/configuration/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig {
 
         config.setAllowCredentials(true);
         config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("https://peter6706.c44.integrator.host/");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
