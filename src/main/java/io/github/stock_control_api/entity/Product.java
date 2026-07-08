4d0c9cc26bc6fa46d88a319f45b5db52eac30c93
Seu Nome <petersomodair@gmail.com>
2026-07-08 12:49:21 -0300

    feat: configuração com banco estabelecida, flyway configurado e entidade Product mapeada

--- src/main/java/io/github/stock_control_api/entity/Product.java
+++ src/main/java/io/github/stock_control_api/entity/Product.java
@@ -1,11 +1,23 @@
 package io.github.stock_control_api.entity;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "tb_product")
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 255, nullable = false)
     private String name;
+
+    @Column(length = 255, nullable = false)
     private String code;
+
+    @Column(precision = 8, scale = 2)
     private BigDecimal price;
 }
