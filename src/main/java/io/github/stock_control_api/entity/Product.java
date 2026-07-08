0f6f596a73faa4c7e06042a753ea62f20d1e70cb
Seu Nome <petersomodair@gmail.com>
2026-07-08 12:30:28 -0300

    feat: foi criado um POJO de Product

---
+++ src/main/java/io/github/stock_control_api/entity/Product.java
@@ -0,0 +1,11 @@
+package io.github.stock_control_api.entity;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private String code;
+    private BigDecimal price;
+}
