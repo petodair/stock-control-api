6547b27eb394d24183b265935522391f1af90fc3
Seu Nome <petersomodair@gmail.com>
2026-07-18 02:37:24 -0300

    feat: foi criado o validador de ProductType

--- src/main/java/io/github/stock_control_api/validate/ProductTypeValidate.java
+++ src/main/java/io/github/stock_control_api/validate/ProductTypeValidate.java
@@ -1,4 +1,20 @@
 package io.github.stock_control_api.validate;
 
+import io.github.stock_control_api.exception.producttype.ProductTypeAlreadyException;
+import io.github.stock_control_api.repository.ProductTypeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
 public class ProductTypeValidate {
+
+    private final ProductTypeRepository productTypeRepository;
+
+    private void existsByName(String name) {
+        if(this.productTypeRepository.existsByName(name)){
+            throw new ProductTypeAlreadyException("Já existe um tipo de produto cadastrado " +
+                    "com esse nome");
+        }
+    }
 }
