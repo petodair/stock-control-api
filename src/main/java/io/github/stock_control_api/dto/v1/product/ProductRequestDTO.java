0f1907730fa0b6aa8976fcc441925acd538e702f
Seu Nome <petersomodair@gmail.com>
2026-07-19 17:57:05 -0300

    feat: campo productType adicionado ao dto de requisicao de Product

--- src/main/java/io/github/stock_control_api/dto/v1/product/ProductRequestDTO.java
+++ src/main/java/io/github/stock_control_api/dto/v1/product/ProductRequestDTO.java
@@ -1,10 +1,13 @@
 package io.github.stock_control_api.dto.v1.product;
 
+import io.github.stock_control_api.entity.ProductType;
+
 import java.math.BigDecimal;
 
 public record ProductRequestDTO(
         String name,
         String code,
-        BigDecimal price
+        BigDecimal price,
+        ProductType productType
 ) {
 }
