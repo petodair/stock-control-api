1072603e42c66ad1edf92cc5b8433cb99adb6c1c
Seu Nome <petersomodair@gmail.com>
2026-07-19 13:56:22 -0300

    feat: criados dtos de requisicao e de resposta da entidade ProductType

---
+++ src/main/java/io/github/stock_control_api/dto/v1/producttype/ProductTypeRequestDTO.java
@@ -0,0 +1,6 @@
+package io.github.stock_control_api.dto.v1.producttype;
+
+public record ProductTypeRequestDTO(
+        String name
+) {
+}
---
+++ src/main/java/io/github/stock_control_api/dto/v1/producttype/ProductTypeResponseDTO.java
@@ -0,0 +1,7 @@
+package io.github.stock_control_api.dto.v1.producttype;
+
+public record ProductTypeResponseDTO(
+        Long id,
+        String name
+) {
+}
