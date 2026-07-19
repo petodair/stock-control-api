f4f3e8115c041120e0e0dafc7472984de97b4124
Seu Nome <petersomodair@gmail.com>
2026-07-19 15:00:04 -0300

    feat: repository de produto alterado primeiro para fazer fecth com ProductType, segundo para usar specifications

--- src/main/java/io/github/stock_control_api/repository/ProductRepository.java
+++ src/main/java/io/github/stock_control_api/repository/ProductRepository.java
@@ -2,7 +2,15 @@ package io.github.stock_control_api.repository;
 
 import io.github.stock_control_api.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface ProductRepository extends JpaRepository<Product,Long> {
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product,Long>, JpaSpecificationExecutor<Product> {
     boolean existsByCode(String code);
+
+    @Query("SELECT p FROM Product p JOIN FETCH p.productType pt WHERE pt.name = :typeName")
+    List<Product> findByProductTypeName(@Param("typeName") String typeName);
 }
