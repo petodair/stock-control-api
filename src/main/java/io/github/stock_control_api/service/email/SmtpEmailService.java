83006f21e1186dc6fc31e8fbc894f808d957e82d
Seu Nome <petersomodair@gmail.com>
2026-07-25 00:58:32 -0300

    feat: eu havia esquecido de marcar o service com a anotacao certa

--- src/main/java/io/github/stock_control_api/service/email/SmtpEmailService.java
+++ src/main/java/io/github/stock_control_api/service/email/SmtpEmailService.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
+@Service
 public class SmtpEmailService implements EmailService {
 
     private final JavaMailSender mailSender;
